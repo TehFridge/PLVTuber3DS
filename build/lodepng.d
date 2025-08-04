ec99f56ce8cdffa26dc6877927d3fd3c98c99899
U-TEHFRIDGE-PC\karol <karolwilk75@gmail.com>
2025-08-04 14:57:25 +0200

    readme lel

--- build/lodepng.d
+++
@@ -1,4 +0,0 @@
-lodepng.o: \
- C:/Users/karol/Desktop/syf/Projekty/VTUBERPL_3DS/source/lodepng.c \
- C:/Users/karol/Desktop/syf/Projekty/VTUBERPL_3DS/source/lodepng.h
-C:/Users/karol/Desktop/syf/Projekty/VTUBERPL_3DS/source/lodepng.h:
